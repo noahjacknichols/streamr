626b2f737d3d57411f5d9916a0cc2e764c2f0e68
noahjacknichols <noahjacknichols@gmail.com>
2020-12-23 23:10:05 -0500

    removing from base

--- routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-const auth = express.Router();
-const authController = require('../controllers/authController');
-
-auth.post('/login', authController.login);
-
-auth.post('/create', authController.create);
-
-module.exports = auth;
--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const { resourceLimits } = require('worker_threads');
-const express = require('express');
-const routes = express.Router();
-
-const authRouter = require('./authRouter');
-
-routes.use('/', authRouter);
-
-module.exports = routes;
