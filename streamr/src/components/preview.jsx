5d65b789f3320d47e145fbeb6be02b296e8fa67d
noahjacknichols <noahjacknichols@gmail.com>
2020-12-27 16:42:01 -0500

    added onhover video playing to previews

--- streamr/src/components/preview.jsx
+++ streamr/src/components/preview.jsx
@@ -3,19 +3,35 @@ import { Row, Col, Container } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom'
 import '../css/video.css';
 class VideoPlayerPreview extends React.Component {
+    playerRef = React.createRef();
     constructor(props) {
         super();
         this.toVideo = this.toVideo.bind(this);
-        this.playPause = this.playPause.bind(this);
+        this.vidPlay = this.vidPlay.bind(this);
+        this.vidPause = this.vidPause.bind(this);
     }
 
     toVideo() {
         this.props.history.push("/video");
     }
     
-    playPause() {
-        console.log(this.refs);
-        this.refs.vidRef.current.play();
+    vidPlay() {
+        let vid = this.playerRef.current;
+        console.log("play");
+        if(vid.paused) {
+            vid.play();
+        }else {
+            // vid.pause();
+        }
+        // this.playerRef.current.play();
+    }
+
+    vidPause() {
+        let vid = this.playerRef.current;
+        console.log("pause");
+        if(!vid.paused) {
+            vid.pause();
+        }
     }
     
     render() {
@@ -26,16 +42,17 @@ class VideoPlayerPreview extends React.Component {
                             Evangelion
                         </div>
                         <div className="">
-                            <video className="video-container-preview" muted preload="metadata">
+                            <video className="video-container-preview" ref={this.playerRef}
+                                onMouseEnter={this.vidPlay} onMouseLeave={this.vidPause}  preload="metadata">
                                 <source src="http://localhost:5000/video/stream#t=145" type="video/mp4"></source>
                                 <track src="http://localhost:5000/video/stream" kind="subtitles" srcLang="en"></track>
                             </video>
                         </div>
                 </div>
 
-                <div className="video-play-button">
+                <div className="video-play-button shadow">
                     
-                    <i onClick={this.toVideo}className="fas fa-play video-play-icon"></i>
+                    <i onClick={this.toVideo}className="video-play-icon fas fa-play "></i>
                 </div>
             </div>
         )
