465aeb52abaf4151fc55df46cc121271136182c8
noahjacknichols <noahjacknichols@gmail.com>
2020-12-27 00:30:55 -0500

    last commit for td, adding some pages and components and styling

--- streamr/src/components/video.jsx
+++ streamr/src/components/video.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import '../css/video.css';
-export default class VideoPlayer extends React.Component {
+export default class Video extends React.Component {
     
     render() {
         return(
             <Container>
                 <Row>
-                <Col xs = {12}>
+                <Col xs={12}>
                     <div className="video-background">
-                    <video className="video-container" controls>
+                    <video className="video-container" controls autoPlay>
                         <source src="http://localhost:5000/video/stream" type="video/mp4"></source>
-                        <track src="http://localhost:5000/video/stream" kind="subtitles" srcLang="en"></track>
                     </video>
                     </div>
                 </Col>
