0defb47791d08f74be2c1a2cde8f9bed8c282427
Noah Nichols <noahjacknichols@gmail.com>
2021-02-14 02:12:01 -0500

    Updating gitignore and added mroe functions for processing videos

--- mediaConverter/app.py
+++ mediaConverter/app.py
@@ -24,15 +24,30 @@ def create_bucket(bucket_name, region):
 def get_files_in_bucket(bucket):
     return [bucket_obj for bucket_obj in bucket.objects.all()]
 
-def resize_video(video, settings):
-    if('.mkv' in video.key):
-        print()
-        
+def resize_video(video_path, width):
+    if(os.path.isfile(video_path)):
+        input_vid = ffmpeg.input(video_path)
+        video_name = video_path.split('.')[0]
+        print('vid:', video_name)
+        vid = (
+            input_vid
+            .filter('scale', width, -2)
+            .output('./temp/out/' + 'test', f="mp4", segment_time='30')
+            .run()
+            )
+        return True
+
+def download_file(bucket, video, settings):
+        bucket.download_file(video.key, './temp/in/' + video.key)
+        resize_video('./temp/in/' + video.key, '720')
+
 
 def main():
     s3 = get_s3('us-east-1')
     allBuckets = list_my_buckets(s3)
     for bucketName in allBuckets.keys():
         print('bucketName:', get_files_in_bucket(allBuckets[bucketName]))
+        for video in get_files_in_bucket(allBuckets[bucketName]):
+            download_file(allBuckets[bucketName], video, {})
 
 main()
